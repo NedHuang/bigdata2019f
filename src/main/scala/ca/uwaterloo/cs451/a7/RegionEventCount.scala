e938c7b87aacf24b280df5699fe2755f4f125acd
m78huang <m78huang@student.cs.uwaterloo.ca>
2019-12-02 23:53:00 -0500

    add a7 files

---
+++ src/main/scala/ca/uwaterloo/cs451/a7/RegionEventCount.scala
@@ -0,0 +1,158 @@
+/**
+  * Bespin: reference implementations of "big data" algorithms
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+package ca.uwaterloo.cs451.a7
+
+import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.apache.log4j._
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{ManualClockWrapper, Minutes, StreamingContext}
+import org.apache.spark.streaming.scheduler.{StreamingListener, StreamingListenerBatchCompleted}
+import org.apache.spark.util.LongAccumulator
+import org.rogach.scallop._
+
+import scala.collection.mutable
+
+class RegionEventCountConf(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input, checkpoint, output)
+  val input = opt[String](descr = "input path", required = true)
+  val checkpoint = opt[String](descr = "checkpoint path", required = true)
+  val output = opt[String](descr = "output path", required = true)
+  verify()
+}
+
+object RegionEventCount {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]): Unit = {
+    val args = new RegionEventCountConf(argv)
+
+    log.info("Input: " + args.input())
+
+    val spark = SparkSession
+      .builder()
+      .config("spark.streaming.clock", "org.apache.spark.util.ManualClock")
+      .appName("RegionEventCount")
+      .getOrCreate()
+
+    val numCompletedRDDs = spark.sparkContext.longAccumulator("number of completed RDDs")
+
+    val batchDuration = Minutes(1)
+    val ssc = new StreamingContext(spark.sparkContext, batchDuration)
+    val batchListener = new StreamingContextBatchCompletionListener(ssc, 24)
+    ssc.addStreamingListener(batchListener)
+
+    // latitude and longtitude
+    val goldman_longitude = List(-74.0141012,-74.013777,-74.0141027,-74.0144185)
+    val goldman_latitude = List(40.7152191,40.7152275,40.7138745,40.7140753)
+    val goldman_longitude_max =  goldman_longitude.max
+    val goldman_longitude_min = goldman_longitude.min
+    val goldman_latitude_max = goldman_latitude.max
+    val goldman_latitude_min = goldman_latitude.min
+
+    val citigroup_longitude = List(-74.011869,-74.009867,-74.010140,-74.012083)
+    val citigroup_latitude = List(40.7217236,40.721493,40.720053,40.720267)
+    val citigroup_longitude_max =  citigroup_longitude.max
+    val citigroup_longitude_min = citigroup_longitude.min
+    val citigroup_latitude_max = citigroup_latitude.max
+    val citigroup_latitude_min = citigroup_latitude.min
+
+
+    val rdds = buildMockStream(ssc.sparkContext, args.input())
+    val inputData: mutable.Queue[RDD[String]] = mutable.Queue()
+    val stream = ssc.queueStream(inputData)
+
+    val wc = stream.map(_.split(","))
+      .map(line => {
+          if(line(0) == "yellow")
+          {
+              (line(10).toDouble, line(11).toDouble)
+          }else
+          {
+              (line(8).toDouble, line(9).toDouble)
+          }
+      })
+      .filter(line => 
+        ((line._1 > goldman_longitude_min) && (line._1 < goldman_longitude_max) && (line._2 > goldman_latitude_min) && (line._2 < goldman_latitude_max))
+        ||
+        ((line._1 > citigroup_longitude_min) && (line._1 < citigroup_longitude_max) && (line._2 > citigroup_latitude_min) && (line._2 < citigroup_latitude_max))
+      )
+      // .foreach(pintln)
+      .map(line => {
+          if((line._1 > goldman_longitude_min) && (line._1 < goldman_longitude_max) && (line._2 > goldman_latitude_min) && (line._2 < goldman_latitude_max))
+          {
+              ("goldman", 1)
+          }else{
+              ("citigroup", 1)
+          }
+      })
+      .reduceByKeyAndWindow(
+        (x: Int, y: Int) => x + y, (x: Int, y: Int) => x - y, Minutes(60), Minutes(60))
+      .persist()
+
+    wc.print()
+    wc.saveAsTextFiles(args.output())
+
+    wc.foreachRDD(rdd => {
+      numCompletedRDDs.add(1L)
+    })
+    ssc.checkpoint(args.checkpoint())
+    ssc.start()
+
+    for (rdd <- rdds) {
+      inputData += rdd
+      ManualClockWrapper.advanceManualClock(ssc, batchDuration.milliseconds, 50L)
+    }
+
+    batchListener.waitUntilCompleted(() =>
+      ssc.stop()
+    )
+  }
+
+  class StreamingContextBatchCompletionListener(val ssc: StreamingContext, val limit: Int) extends StreamingListener {
+    def waitUntilCompleted(cleanUpFunc: () => Unit): Unit = {
+      while (!sparkExSeen) {}
+      cleanUpFunc()
+    }
+
+    val numBatchesExecuted = new AtomicInteger(0)
+    @volatile var sparkExSeen = false
+
+    override def onBatchCompleted(batchCompleted: StreamingListenerBatchCompleted) {
+      val curNumBatches = numBatchesExecuted.incrementAndGet()
+      log.info(s"${curNumBatches} batches have been executed")
+      if (curNumBatches == limit) {
+        sparkExSeen = true
+      }
+    }
+  }
+
+  def buildMockStream(sc: SparkContext, directoryName: String): Array[RDD[String]] = {
+    val d = new File(directoryName)
+    if (d.exists() && d.isDirectory) {
+      d.listFiles
+        .filter(file => file.isFile && file.getName.startsWith("part-"))
+        .map(file => d.getAbsolutePath + "/" + file.getName).sorted
+        .map(path => sc.textFile(path))
+    } else {
+      throw new IllegalArgumentException(s"$directoryName is not a valid directory containing part files!")
+    }
+  }
+}
